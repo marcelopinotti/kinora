a77651e1699215ad014453f5a70d61f0f76082e8
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-07-27 18:37:52 -0300

    feat: add Streaming entity with JPA annotations

---
+++ src/main/java/com/kinora/domain/Streaming.java
@@ -0,0 +1,24 @@
+package com.kinora.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "streaming")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Streaming {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String nome;
+
+
+
+}
