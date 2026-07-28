10a3712428794969c3fbe33125dc622d89304bda
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-07-28 00:00:35 -0300

    fix: remove unnecessary annotation from CategoriaRepository

--- src/main/java/com/kinora/repository/CategoriaRepository.java
+++ src/main/java/com/kinora/repository/CategoriaRepository.java
@@ -4,7 +4,7 @@ import com.kinora.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 }
  
