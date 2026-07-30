303f1f1a7aaeb10efc15dbba2ae6aa7f145ed9f1
Marcelo Pinotti <marcelopinotti@protonmail.com>
2026-07-30 12:12:24 -0300

    feat: add method to FilmeRepository for fetching filmes by categoriaId

--- src/main/java/com/kinora/repository/FilmeRepository.java
+++ src/main/java/com/kinora/repository/FilmeRepository.java
@@ -3,5 +3,9 @@ package com.kinora.repository;
 import com.kinora.domain.Filme;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FilmeRepository extends JpaRepository<Filme, Long> {
+    List<Filme> findByCategorias_Id(Long categoriaId);
+
 }
